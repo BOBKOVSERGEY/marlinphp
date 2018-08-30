6904ed33f677f86f049a764e4c97209696179d5d
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-08-30 16:01:33 +0300

    CRUD

---
+++ level-1/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//trim();
+//ltrim();
+
+echo ltrim('some,', ',');
+
+$data = [
+  'title' => 'hello world',
+  'content' => 'Заголовок'
+];
+
+// SELECT * FROM tasks
+// INSERT INTO tasks (title, content) VALUE('Заголовок', 'Контент')
+// UPDATE tasks SET title='Заголовок' WHERE id=5;
+// DELETE FROM tasks WHERE id=5
